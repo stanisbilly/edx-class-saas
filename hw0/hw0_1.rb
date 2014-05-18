c9209562b23a9ac4517b81fd6d1e08473931917e
Stanis Billy <stanisbilly@gmail.com>
2014-05-17 21:58:02 -0300

    Updated assignment descriptions

--- hw0/hw0_1.rb
+++ hw0/hw0_1.rb
@@ -1,10 +1,11 @@
 =begin
 HW0 PART 1-------------------------
-Define a method sum wich takes an arr of ints as an arg and returns sum of its elems. For an emtpy arr it should return 0.
 
-Define a method max_2_sum, which takes an arr of ints as an arg and returns the sum of its two largest elems. For an emtpy arr it should return 0. For an arr with just one elem, it should return that elem.
+Define a method sum which takes an array of integers as an argument and returns the sum of its elements. For an empty array it should return zero.
 
-Define a method, sum_to_n? which takes an arr of ints and an additonal int, n, as args and returns true if any of two distinct elems int the arr of ints sum to n. An empty arr or single elem arr should both return false.
+Define a method max_2_sum which takes an array of integers as an argument and returns the sum of its two largest elements. For an empty array it should return zero. For an array with just one element, it should return that element.
+
+Define a method sum_to_n? which takes an array of integers and an additional integer, n, as arguments and returns true if any two distinct elements in the array of integers sum to n. An empty array or single element array should both return false.
 =end
 
 def sum(nums)
--- hw0/hw0_3.rb
+++ hw0/hw0_3.rb
@@ -1,8 +1,9 @@
 =begin
 HW0 PART 3-------------------------
-Define a class BookInStock which represents a book with an isbn number, isbn, and a price of the book as a floating-pt num, price, as attrs. The constructor should accept the ISBN num (a str) as the first arg and price as second arg, and should raise ArgumentError if the ISBN num is the empty string or if the price is <= 0.
 
-Include the proper getters and setters for these attrs. Include a method price_as_string that returns the price of the book with a leading dollar sign and trailing zeros (eg. price of 20 should display as "$20.00" and price of 33.8 should return "$33.80". 
+Define a class BookInStock which represents a book with an isbn number, isbn, and price of the book as a floating-point number, price, as attributes. The constructor should accept the ISBN number (a string) as the first argument and price as second argument, and should raise ArgumentError (one of Ruby's built-in exception types) if the ISBN number is the empty string or if the price is less than or equal to zero.
+
+Include the proper getters and setters for these attributes. Include a method price_as_string that returns the price of the book with a leading dollar sign and trailing zeros, that is, a price of 20 should display as "$20.00" and a price of 33.8 should display as "$33.80".
 =end
 
 class BookInStock
