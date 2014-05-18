80dd70b8904a832b4a62be2ec996003dd1ce3f83
Stanis Billy <stanisbilly@gmail.com>
2014-05-18 03:29:28 -0300

    attr_accessor_with_history done

--- hw1/lib/attr_accessor_with_history.rb
+++ hw1/lib/attr_accessor_with_history.rb
@@ -4,7 +4,21 @@ class Class
     attr_reader attr_name # create the attribute's getter
     attr_reader attr_name+"_history" # create bar_history getter
     class_eval %Q{
-      # YOUR CODE HERE
+    	def #{attr_name}; @#{attr_name}; end
+
+    	def #{attr_name}= value
+    		
+    		# initialize if doesn't exist
+				@#{attr_name}_history = [] if @#{attr_name}_history.nil?
+				
+				# add to history
+    		@#{attr_name}_history << #{attr_name}
+
+				# set the value itself
+    		@#{attr_name} = value
+    	end
+
+    	def #{attr_name}_history; @#{attr_name}_history; end
     }
   end
 end
