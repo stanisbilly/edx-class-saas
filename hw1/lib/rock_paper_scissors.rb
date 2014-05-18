98e69b3a64c064f0cb9b13741f20236bb58662a7
Stanis Billy <stanisbilly@gmail.com>
2014-05-18 02:42:40 -0300

    rps problem done

--- hw1/lib/rock_paper_scissors.rb
+++ hw1/lib/rock_paper_scissors.rb
@@ -3,12 +3,26 @@ class RockPaperScissors
   # Exceptions this class can raise:
   class NoSuchStrategyError < StandardError ; end
 
+	
   def self.winner(player1, player2)
-    # YOUR CODE HERE
+  	valid = /[RPS]/
+    raise NoSuchStrategyError.new("Strategy must be one of R,P,S") unless (player1[1] =~ valid) && (player2[1] =~ valid)
+  	p1_wins(player1[1],player2[1]) ? player1 : player2
   end
 
   def self.tournament_winner(tournament)
-    # YOUR CODE HERE
+    return self.winner(tournament[0], tournament[1]) unless tournament[0][0].is_a? Array
+    self.winner(tournament_winner(tournament[0]), self.tournament_winner(tournament[1]))
   end
-
+  
+  # Helper
+	def self.p1_wins(p1, p2)
+		return true if (p1 == p2)
+		if (p1 == 'R' && p2 == 'S') || (p1 == 'S' && p2 == 'P') || (p1 == 'P' && p2 == 'R')
+			return true
+		else
+			return false
+		end
+	end
+	
 end
